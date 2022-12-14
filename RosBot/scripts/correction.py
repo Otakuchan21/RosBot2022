68abd2ebecdf6eba49a38088ab2400caa79536e9
Otakuchan21 <78582741+Otakuchan21@users.noreply.github.com>
2022-12-14 15:28:51 +0000

    service server for reorientation

--- RosBot/scripts/correction.py
+++ RosBot/scripts/correction.py
@@ -4,54 +4,53 @@ import rospy
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry as odom
 from tf.transformations import euler_from_quaternion
+from RosBot.srv import correction, correctionResponse
 import math
-pub = rospy.Publisher("cmd_vel", Twist, queue_size = 10)
+
 class correction():
     def __init__(self):
-        #self.pub = rospy.Publisher("cmd_vel", Twist, queue_size = 10)
+        self.pub = rospy.Publisher("cmd_vel", Twist, queue_size = 10)
         self.msg = Twist()
         self.velocityValue()
+        self.z_deg = 0
 
     def velocityValue(self):
-        self.msg.linear.x = 0.5
+        self.msg.linear.x = 0
         self.msg.linear.y = 0
         self.msg.linear.z = 0
         self.msg.angular.x = 0
         self.msg.angular.y = 0
         self.msg.angular.z = 0
-
-    def decayFunc(self,error):
-        pass
-
+    
     def callback(self,data):
         q_base = data.pose.pose.orientation
         q_list = [q_base.x, q_base.y, q_base.z, q_base.w]
-        print(q_list)
         euler_x, euler_y, euler_z = euler_from_quaternion(q_list)
-        z_deg = math.degrees(euler_z)
-        if z_deg > 92.0 or z_deg < 88.0:
-            if z_deg > 92.0:
-                self.msg.linear.x = 0
-                self.msg.angular.z = -0.5
-            if z_deg < 88.0:
-                self.msg.linear.x = 0
-                self.msg.angular.z = 0.5
-        else:
-            self.velocityValue()
-        print(self.msg)
-        pub.publish(self.msg)
+        self.z_deg = math.degrees(euler_z)
+    
+    def service_cb(self, request):
+        while self.z_deg!<95.0 and self.z_deg!>85.0:
+            sub = rospy.Subscriber('pose', odom, self.callback)
+            if self.z_deg > 95.0 or self.z_deg < 85.0:
+                if z_deg > 95.0:
+                    self.msg.linear.x = 0
+                    self.msg.angular.z = -0.5
+                if z_deg < 85.0:
+                    self.msg.linear.x = 0
+                    self.msg.angular.z = 0.5
+        
+            pub.publish(self.msg)
+        
+        return correctionResponse("reorientation complete", 0)
+        
 
-
+        
+        
 def main():
     rospy.init_node("correction")
     corr = correction()
-    sub = rospy.Subscriber('odom/wheel', odom ,corr.callback)
-    rate = rospy.Rate(1)
-
-    #while not rospy.is_shutdown():
-
+    service = rospy.Service('correction', correction, corr.service_cb)
     rospy.spin()
-
 
 if __name__=="__main__":
     main()
